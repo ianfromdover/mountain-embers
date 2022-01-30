512292327ac3b16a3af55e3cb6a47a5ba08230e7
ian-from-dover <ian09hong@gmail.com>
2022-01-31 01:26:56 +0800

    Make player turn wrt camera, add goal torches.
    Let's make the player actually have a game to play.

--- asgn1/Assets/B_Scripts/CollectFire.cs
+++ asgn1/Assets/B_Scripts/CollectFire.cs
@@ -6,19 +6,32 @@ public class CollectFire : MonoBehaviour
 {
     public GameObject holdingTorch;
     public GameObject campfire;
+    public GameObject[] torchesAtCampfire;
+    private static int maxTorches = 6;
+    private static int noOfTorches = 0;
+    private static int torchesLeft = maxTorches - noOfTorches;
 
+    public int getTorchesLeft() {
+        return torchesLeft;
+    }
+    
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Fire Stick"))
+        if (other.gameObject.CompareTag("Fire Stick") && !holdingTorch.activeSelf)
         {
             other.gameObject.SetActive(false);
             holdingTorch.SetActive(true);
         }
         else if (other.gameObject.CompareTag("Campfire")) 
         {
-            holdingTorch.SetActive(false);
             // make campfire bigger
-            // UI noOfSticksLeft--;
+            holdingTorch.SetActive(false);
+
+            if (noOfTorches < maxTorches) {
+                torchesAtCampfire[noOfTorches].SetActive(true);
+                noOfTorches++;
+                torchesLeft--; // to update UI
+            }
             // UI make targeting reticle
         }
     }
--- asgn1/Assets/B_Scripts/PlayerControl.cs
+++ asgn1/Assets/B_Scripts/PlayerControl.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    /**
+     * The player and the sphere are siblings, but the player uses
+     * the sphere to move. This script links them.
+     * 
+     * Moving the player relative to the camera was referenced from Okido from
+     * https://forum.unity.com/threads/moving-character-relative-to-camera.383086/
+     */
     private Rigidbody rb;
     public float speed;
+    public float turnSpeed;
+    public float boostMultiplier;
 
-    // experimental
     public GameObject sphere;
-    private Vector3 offset;
+    private Rigidbody sphereRb;
+    private Vector3 sphereOffsetToParent;
 
     void Start()
     {
-        rb = sphere.GetComponent<Rigidbody>();
-        offset = transform.position - sphere.transform.position;
+        rb = GetComponent<Rigidbody>();
+        sphereRb = sphere.GetComponent<Rigidbody>();
+        sphereOffsetToParent = transform.position - sphere.transform.position;
 
         // Lock player's mouse to game
         Cursor.lockState = CursorLockMode.Locked;
@@ -26,17 +36,30 @@ public class PlayerControl : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        bool isBoosting = Input.GetButton("Boost");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        rb.AddForce(movement * speed);
 
-        // friction
-        rb.AddForce(new Vector3(moveHorizontal, -1.0f, moveVertical));
-        
+        Vector3 dir = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 camDirection = Camera.main.transform.rotation * dir; // gets all 3 axes of camera
+        Vector3 tgtDirection = new Vector3(camDirection.x, 0, camDirection.z); // only move horizontally
+
+        if (dir != Vector3.zero) { // when got input, turn sphere to face camera's direction
+            // Dampen from current rotation to target rotation
+            sphere.transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                  Quaternion.LookRotation(tgtDirection),
+                                                  Time.deltaTime * turnSpeed);
+        }
+
+        // translate forwards
+        float boost = isBoosting ? boostMultiplier : 1;
+        sphereRb.AddForce(tgtDirection.normalized * speed * boost); // normalized prevents extra speed when diagonal input is present
+
+        // rotate the character to face the camera direction
         transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
     }
 
     void LateUpdate() {
-        transform.position = sphere.transform.position + offset;
+        // move model body to the sphere
+        transform.position = sphere.transform.position + sphereOffsetToParent;
     }
 }
